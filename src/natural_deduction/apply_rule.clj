4ebe6ce6befaa4d5fca01395ed78419fc3270355
kuerschten <dk-siggi@web.de>
2014-08-05 13:10:47 +0200

    Changed "reform" to "rewrite" in apply_rule.clj

--- src/natural_deduction/apply_rule.clj
+++ src/natural_deduction/apply_rule.clj
@@ -11,18 +11,18 @@
         (throw (IllegalArgumentException. (str "The variable \"" new-var "\" that shall be inserted already exists in \"" pred"\".")))
         (clojure.walk/prewalk-replace {var new-var} pred)))))
 
-(defn- reform
-  "Reform a rule form. (It is needed to use apply-rule-reform)
+(defn- rewrite
+  "Rewrite a rule form. (It is needed to use apply-rule-rewrite)
    Returns a String."
   [form quoted?]
-  (let [reformed-form (when (coll? form) (map #(reform % true) form))]
+  (let [rewrited-form (when (coll? form) (map #(rewrite % true) form))]
     (cond
       (= \$ (first (str form))) (if quoted? (list 'list form) form)
-      (coll? form) (if quoted? (list 'list (list 'seq (concat '(concat) reformed-form))) (list 'seq (concat '(concat) (map #(reform % true) form))))
+      (coll? form) (if quoted? (list 'list (list 'seq (concat '(concat) rewrited-form))) (list 'seq (concat '(concat) (map #(rewrite % true) form))))
       :else (if quoted? (list 'list (list 'quote form)) (list 'quote form)))))
 
-(defn- apply-rule-reform
-  "Reform an unreformed(!) rule form to function that uses core.logic.
+(defn- apply-rule-rewrite
+  "Rewrite an unrewrited(!) rule form to function that uses core.logic.
    Returns a String."
   [rule]
   (let [args (:args rule)
@@ -32,11 +32,11 @@
                             (= \$ (first (str %)))
                             (not (contains? (set args) %)))
                          (flatten forms))))
-        equals (map #(list 'clojure.core.logic/== (reform (first %) false) (reform (second %) false)) forms)]
+        equals (map #(list 'clojure.core.logic/== (rewrite (first %) false) (rewrite (second %) false)) forms)]
     (list 'fn args (concat (list 'clojure.core.logic/fresh) (list vars) equals))))
 
 (defn- apply-rule-1step
-  "Use an unreformed(!) rule on terms.
+  "Use an unrewrited(!) rule on terms.
    The rule can be used foreward or backward (flag foreward?).
    Terms is a collection of all terms.
    Return the result of the therms while using the rule."
@@ -45,7 +45,7 @@
    (when (and
            movement
            (= (inc (count terms)) (count (:args rule))))
-     (let [r (apply-rule-reform rule)
+     (let [r (apply-rule-rewrite rule)
            h1 (replace {movement 'q} (:args rule))
            args (replace (zipmap (filter #(not= 'q %) h1) (map #(list 'quote %) terms)) h1)
            function (list 'clojure.core.logic/run 1 '[q] (concat (list r) args))
@@ -59,13 +59,13 @@
          res)))))
 
 (defn apply-rule-foreward
-  "Use an unreformed(!) rule on terms.
+  "Use an unrewrited(!) rule on terms.
    Return the result of the therms while using the rule foreward."
   [rule & terms]
   (apply-rule-1step true rule terms))
 
 (defn apply-rule-backward
-  "Use an unreformed(!) rule on terms.
+  "Use an unrewrited(!) rule on terms.
    Return the result of the therms while using the rule backward."
   [rule & terms]
   (apply-rule-1step false rule terms))
